29d827f7b0c88f82617dd5e343f8e43a9bbb76af
Soki <soki.fin02@gmail.com>
2023-11-12 15:44:51 +0200

    toml

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,7 +1,9 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, licen, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.licen = licen
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
@@ -9,9 +11,15 @@ class Project:
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
+        authors = ""
+        for i in self.authors:
+            authors += "- " + i + "\n"
+
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
+            f"\nLicense: {self.licen or '-'}"
+            f"\n\nAuthors:\n{authors or '-'}"
             f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\n\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomli
 
 
 class ProjectReader:
@@ -9,7 +10,17 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+
+        toml_dict = tomli.loads(content)
+
+        name = toml_dict["tool"]["poetry"]["name"]
+        description = toml_dict["tool"]["poetry"]["description"]
+        dependencies = toml_dict["tool"]["poetry"]["dependencies"]
+        dev_dependencies = toml_dict["tool"]["poetry"]["group"]["dev"]["dependencies"]
+        licen = toml_dict["tool"]["poetry"]["license"]
+        authors = toml_dict["tool"]["poetry"]["authors"]
+
+        
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, licen, authors, dependencies, dev_dependencies)
