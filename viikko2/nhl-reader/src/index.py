b02cb6166120bfee8aef0b08623f8a4cb706ae99
Soki <soki.fin02@gmail.com>
2023-11-12 17:39:00 +0200

    vk2 t.12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -3,30 +3,37 @@ from player import Player
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
-
-    #print("JSON-muotoinen vastaus:")
-    #print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    print("Players from FIN")
-    print()
-
-    fin_players = []
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
-        if player.nationality == "FIN":
-            fin_players.append(player)
-    
-    fin_players = sorted(fin_players, key=lambda x: x.goals + x.assists, reverse=True)
-    
-    for player in fin_players:
         print(player)
+
+
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json() 
+        players = [Player(player_dict) for player_dict in response]
+        return players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players = self.reader.get_players()
+
+        nationality_players = []
+        for player in players:
+            if player.nationality == nationality:
+                nationality_players.append(player)
+        nationality_players = sorted(nationality_players, key=lambda x: x.goals + x.assists, reverse=True)
+        return nationality_players
 
 if __name__ == "__main__":
     main()
